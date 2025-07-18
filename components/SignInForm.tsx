241afe95677c3eb2ae69870afd742e4776d16577
User <yuko.brandon13@gmail.com>
2025-07-18 11:49:53 +0500

    added suspense

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -1,70 +1,66 @@
-'use client'
-import React, { useState } from 'react'
+"use client";
+import React, { Suspense, useState } from "react";
 import classes from "@/app/signIn/page.module.css";
-import Image from 'next/image';
-import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import GoogleButton from './GoogleButton';
-import Link from 'next/link';
+import Image from "next/image";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import GoogleButton from "./GoogleButton";
+import Link from "next/link";
 function SignInForm() {
-    const router = useRouter()
-    const [error,setError] = useState<string | null | undefined >("")
-    const formHandler:React.FormEventHandler<HTMLFormElement> = async(e)=>{
-        e.preventDefault()
-        const formData = new FormData(e.currentTarget);
-        const res = await signIn("credentials",{
-            email:formData.get("email"),
-            password:formData.get("password"),
-            redirect:false,
-        })
-        if(res && !res.error){
-            router.push("/profile")
-        }
-        else{
-            setError(res?.error)
-        }
+  const router = useRouter();
+  const [error, setError] = useState<string | null | undefined>("");
+  const formHandler: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const res = await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
+    if (res && !res.error) {
+      router.push("/profile");
+    } else {
+      setError(res?.error);
     }
+  };
 
-    return (
-        <main className={classes.main}>
+  return (
+    <Suspense>
+      <main className={classes.main}>
         <div className={classes.left}>
-            <Image
+          <Image
             src={"/Group.svg"}
             alt="todo-image"
             width={282}
             height={285}
-            ></Image>
-            <h3>Организуйте свои задачи здесь!</h3>
-            <p>Практично, быстро и бесплатно!</p>
+          ></Image>
+          <h3>Организуйте свои задачи здесь!</h3>
+          <p>Практично, быстро и бесплатно!</p>
         </div>
         <div className={classes.right}>
-            <GoogleButton/>
-            <form onSubmit={formHandler}>
+          <GoogleButton />
+          <form onSubmit={formHandler}>
             <label>Почта</label>
-            <input
-                type="email"
-                placeholder="Почта"
-                name='email'
-                required
-            />
+            <input type="email" placeholder="Почта" name="email" required />
             <label>Пароль</label>
             <input
-                type="password"
-                placeholder="Пароль"
-                required
-                name='password'
-                minLength={8}
+              type="password"
+              placeholder="Пароль"
+              required
+              name="password"
+              minLength={8}
             />
             <div className={classes.buttons}>
-                <Link href="/register">Создать аккаунт</Link>
-                <button type='submit'>Подтвердить</button>
+              <Link href="/register">Создать аккаунт</Link>
+              <button type="submit">Подтвердить</button>
             </div>
 
             {error && <p className={classes.error}>{error}</p>}
-            </form>
+          </form>
         </div>
-        </main>
-    )
+      </main>
+    </Suspense>
+  );
 }
 
-export default SignInForm
+export default SignInForm;
